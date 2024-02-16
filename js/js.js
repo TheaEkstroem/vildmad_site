d1e7bb249502593fa7c289e4dfcbd676546ea8b0
Thea <thea.frese@gmail.com>
2024-02-16 10:54:10 +0100

    .

--- js/js.js
+++ js/js.js
@@ -9,17 +9,21 @@
 })
  */
 
-fetch("https://qmsbtkywuyypiccoicuh.supabase.co/rest/v1/vildmad?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtc2J0a3l3dXl5cGljY29pY3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTIwNjIsImV4cCI6MjAyMzM4ODA2Mn0.ruBzrTmBPlUx5T2mRJdyOT7s0dFnzWJ2DGvaUyf8dQg")
 
+const urlParams = new URLSearchParams(window.location.search);
+const productsURL = `https://qmsbtkywuyypiccoicuh.supabase.co/rest/v1/vildmad?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtc2J0a3l3dXl5cGljY29pY3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTIwNjIsImV4cCI6MjAyMzM4ODA2Mn0.ruBzrTmBPlUx5T2mRJdyOT7s0dFnzWJ2DGvaUyf8dQg`;
 
+fetch(productsURL)
 .then(res =>res.json())
 .then(showData)
 
-function showData(items){
-    console.log(items);
-    // items.forEach()
+function showData(data){
+    console.log(data);
+    data.forEach(item=>{
+
+        klon.querySelector("h2").textContent = item.title;
+    })
+
 
 }
 
-
-//function (){}
