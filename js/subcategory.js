c7c439c7a97cff53c84e69ef0adc662fbaf8b6be
Thea <thea.frese@gmail.com>
2024-02-19 11:55:13 +0100

    javascript rettelser

--- js/subcategory.js
+++ js/subcategory.js
@@ -1,9 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
-const productsURL = `https://qmsbtkywuyypiccoicuh.supabase.co/rest/v1/vildmadData?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtc2J0a3l3dXl5cGljY29pY3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTIwNjIsImV4cCI6MjAyMzM4ODA2Mn0.ruBzrTmBPlUx5T2mRJdyOT7s0dFnzWJ2DGvaUyf8dQg`;
+const productsURL = "https://qmsbtkywuyypiccoicuh.supabase.co/rest/v1/vildmadData";
 
-fetch(productsURL)
-    .then((response) => response.json())
-    .then(showData);
+   
+fetch(productsURL, {
+  method: "GET",
+headers: {
+apikey:
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtc2J0a3l3dXl5cGljY29pY3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTIwNjIsImV4cCI6MjAyMzM4ODA2Mn0.ruBzrTmBPlUx5T2mRJdyOT7s0dFnzWJ2DGvaUyf8dQg",
+},
+})
+.then((res) => res.json())
+.then(showData);
 
 function showData(dataset) {
   // Looper og kalder funktionen showProduct
@@ -24,15 +31,9 @@ dataClone.querySelector(".sankelandskab").textContent = data.sankelandskab_title
 dataClone.querySelector(".seasons").textContent = data.seasons;
 dataClone.querySelector(".info").textContent = data.category_info
 
-/* dataClone.querySelector(".img").src = 
+/* dataClone.querySelector(".img").src = data.img; */
 
-
-data.img; */
-
-
-
- // Appende
- productList = document.querySelector(".productList");
- productList.appendChild(dataClone);
-
+// Appende
+productList = document.querySelector(".productList");
+productList.appendChild(dataClone);
 }
