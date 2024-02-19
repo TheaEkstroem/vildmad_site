c9211b0bb233fae79134d5fa24eae346d4b01988
Sofie Andersson <sofiesofieandersson@gmail.com>
2024-02-19 15:33:17 +0100

    update category js

--- js/category.js
+++ js/category.js
@@ -1,18 +1,25 @@
-
 // Fetcher dataen fra URL'en
 fetch("https://qmsbtkywuyypiccoicuh.supabase.co/rest/v1/vildmadData?category", {
   method: "GET",
   headers: {
-    apikey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtc2J0a3l3dXl5cGljY29pY3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTIwNjIsImV4cCI6MjAyMzM4ODA2Mn0.ruBzrTmBPlUx5T2mRJdyOT7s0dFnzWJ2DGvaUyf8dQg",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtc2J0a3l3dXl5cGljY29pY3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MTIwNjIsImV4cCI6MjAyMzM4ODA2Mn0.ruBzrTmBPlUx5T2mRJdyOT7s0dFnzWJ2DGvaUyf8dQg",
   },
 })
-.then((response) => response.json())
-.then(showCategories);
+  .then((response) => response.json())
+  .then(showCategories);
 
 function showCategories(categories) {
-    console.log("Categories", categories);
+  console.log("Categories", categories);
 
-    // Looper og kalder funktionen showCategory
-    categories.forEach(showCategory);
+  // Looper og kalder funktionen showCategory
+  categories.forEach(showCategory);
+
+  const template = document.querySelector("template").content;
+
+  const clone = template.cloneNode(true);
+
+  clone.querySelector("a").textContent = categories.category;
+  clone.querySelector("a").href = `subcategory.html?category=${categories.category}`;
+
+  document.querySelector(".categorylist").appendChild(clone);
 }
